d7cbfb9753eba319095286be066c909ee979a56c
SeongJu <sp1799@naver.com>
2022-05-08 17:57:43 +0900

    first commit

---
+++ src/main/java/mediSsok/mediSsokspring/MediSsokSpringApplication.java
@@ -0,0 +1,13 @@
+package mediSsok.mediSsokspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediSsokSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediSsokSpringApplication.class, args);
+	}
+
+}
