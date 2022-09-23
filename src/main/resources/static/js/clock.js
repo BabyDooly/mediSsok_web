d56fd300b2a73b9baaae459f97392ccd8a80e032
JJuceM <dbqlsl1209@naver.com>
2022-09-23 18:52:49 +0900

    알람 modal추가

---
+++ src/main/resources/static/js/clock.js
@@ -0,0 +1,33 @@
+function setClock() {
+    var dateInfo = new Date();
+    var hour = modifyNumber(dateInfo.getHours());
+    var min = modifyNumber(dateInfo.getMinutes());
+    var sec = modifyNumber(dateInfo.getSeconds());
+    var year = dateInfo.getFullYear();
+    var month = dateInfo.getMonth() + 1;
+    var date = dateInfo.getDate();
+    var audio = document.getElementById("alarmaudio");
+    var alarm = document.getElementById("alarmClock");
+    document.getElementById("clocktime").innerHTML = hour + ":" + min + ":" + sec;
+    document.getElementById("clockdate").innerHTML = year + "년" + month + "월" + date + "일";
+
+
+        if (alarm.hasAttribute('aria-hidden') == true) {
+            audio.pause();
+        } else {
+            audio.play();
+        }
+
+}
+
+function modifyNumber(time) {
+    if (parseInt(time) < 10) {
+        return "0" + time;
+    } else
+        return time;
+}
+
+window.onload = function () {
+    setClock();
+    setInterval(setClock, 1000);
+}
